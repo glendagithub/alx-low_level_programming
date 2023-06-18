6543954e4f12c44205fc6ba6d14aa9d8f5a23a77
glendagithub <glandarathakga@gmail.com>
2023-06-18 14:54:55 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,15 +2,19 @@
 
 /**
  * main - Prints the alphabet.
- *
+ *   
  * Return: Always 0 (Success)
- */
+ */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+   char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 
-	for (i = 0; i < 26; i++)				{
-	   putchar(alp[i]);					}
-	putchar('\n');             				return (0);
+   int i;
+   for (i = 0; i < 26; i++)
+   { 
+	   putchar(alphabet[i]);
+						 
+   }		
+   putchar('\n');
+   return (0);
 }
