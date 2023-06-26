6918c5645f690a08dcf489cb0c9312d275147177
glendagithub <glandarathakga@gmail.com>
2023-06-26 16:01:06 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 /**
@@ -8,23 +9,23 @@
  */
 void puts2(char *str)
 {
-	int longi =0;
-	int t =0;
+	int longi = 0;
+	int t = 0;
 	char *y = str;
 	int o;
 
-	while(*y != '\0')
+	while (*y != '\0')
 	{
 		y++;
 		longi++;
 	}
-	t =longi - 1;
+	t = longi - 1;
 	for (o = 0 ; o <= t ; o++)
 	{
 		if (o % 2 == 0)
 		{
-			_putchar(str[o];
-					}
-					}
-					_putchar('\n');
-					}
+			_putchar(str[o]);
+		}
+	}
+	_putchar('\n');
+}
