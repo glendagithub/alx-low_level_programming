34a7604ff182b60d35c31a1bf00d95b7a8a647d8
glendagithub <glandarathakga@gmail.com>
2023-07-26 13:43:22 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,13 +1,15 @@
 #include "dog.h"
 #include <stdlib.h>
-
 /**
- * free_dog -entry point
- * @d: entry structure with pointer
+ * free_dog - frees memory of structure dogs
+ * @d: pointer of structure
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 void free_dog(dog_t *d)
 {
-	if (d != NULL)
+	if (d != 0)
 	{
 		free(d->name);
 		free(d->owner);
