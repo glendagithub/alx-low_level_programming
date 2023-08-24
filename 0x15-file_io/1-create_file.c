5976af0757a62980e3b299564347cf73434d9bab
glendagithub <glandarathakga@gmail.com>
2023-08-24 06:49:41 -0700

    0x15. C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,28 +1,34 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * create_file - creates a file.
- * @filename: name of the file to create.
- * @text_content: is a NULL terminated string to write to the file.
- * Return: 1 on success, -1 on failure
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ * Otherwise - 1.
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor, length = 0;
+	int o, w, len = 0;
 
-	if (!filename)
-		return (-1);
-	file_descriptor = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (file_descriptor < 0)
+	if (filename == NULL)
 		return (-1);
 
-	while (text_content && text_content[length])
-		length++;
-	if (write(file_descriptor, text_content, length) < 0)
+	if (text_content != NULL)
 	{
-		close(file_descriptor);
-		return (-1);
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	close(file_descriptor);
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
 	return (1);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,33 +1,35 @@
-#include "holberton.h"
+#include"main.h"
 
 /**
- * append_text_to_file - appends text at the end of a file.
- * @filename: name of the file to append the text.
- * @text_content: the NULL terminated string to add at the end of the file.
- * Return: 1 on success, -1 on failure
+ *append_text_to_file - appends text at the end of a file
+ *@filename: A pointer to the name of the file.
+ *@text_content: The string to add to the end of the file.
+ *
+ *Return: If the function fails or filename is NULL -1
+ *	if the file does not exist the user lacks write permissions -1
+ *	otherwise 1
  */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file_descriptor, length = 0;
+	int o, w, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-	file_descriptor = open(filename, O_RDWR | O_APPEND);
-	if (file_descriptor < 0)
-		return (-1);
-	if (!text_content)
+
+	if (text_content != NULL)
 	{
-		close(file_descriptor);
-		return (1);
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	while (text_content[length])
-		length++;
-	if (write(file_descriptor, text_content, length) < 0)
-	{
-		close(file_descriptor);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
-	}
-	close(file_descriptor);
+
+	close(o);
+
 	return (1);
 }
